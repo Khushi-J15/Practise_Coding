8309a23e9fe721cd019d2bec0787d9bfaa9a6b74
Khushi Jain <125190832+Khushi-J15@users.noreply.github.com>
2026-01-09 17:45:19 +0530

    .

---
+++ Day-1/leftRotate.java
@@ -0,0 +1,40 @@
+class leftRotate{
+    //Anti-clockwise
+    public static void rotateElemToLeft(int[] arr, int n, int k) {
+    
+        reverse(arr, 0, k-1); // indices
+        reverse(arr, k, n-1);
+        reverse(arr, 0, n-1);
+        
+    }
+
+    public static void reverse(int[] arr, int start, int end){
+        while(start <= end){
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+    public static void main(String args[]) {
+        int[] arr = {1,2,3,4,5,6,7};
+        int n = 7;
+        int k = 2; // defines the number of places by which array is rotated
+
+        for (int i = 0; i < n; i++)
+        System.out.print(arr[i] + " ");
+
+        rotateElemToLeft(arr, n, k);
+
+        System.out.println();        
+        System.out.print("After Rotating the k elements to left ");
+
+        for (int i = 0; i < n; i++)
+        System.out.print(arr[i] + " ");
+
+        System.out.println();
+    }
+
+}
