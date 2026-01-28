0ccfe4123fa15525b565940460c791206863c985
Khushi Jain <125190832+Khushi-J15@users.noreply.github.com>
2026-01-28 21:34:19 +0530

    Create Selection.java

---
+++ Selection.java
@@ -0,0 +1,30 @@
+// selection sort O(n^2)
+
+class Selection{
+    static void printArray(int arr[]){
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args){
+        int arr[] = {7,8,1,3,2};
+
+        for(int i =0; i<arr.length-1;i++){
+            int min = i;
+            for(int j=i+1; j<arr.length; j++){
+                if(arr[j]<arr[min]){
+                    min = j;
+                }
+            }
+            //swap
+            int temp = arr[min];
+            arr[min] = arr[i];
+            arr[i] = temp;
+        }
+        printArray(arr);
+    }
+}
+
+// 1 2 3 7 8 
