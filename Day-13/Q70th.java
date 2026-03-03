4bdbcd881c53996869274529295f1e23748fbc32
Khushi Jain <125190832+Khushi-J15@users.noreply.github.com>
2026-03-03 23:00:05 +0530

    minPartitions method to find the largest digit in the string.

---
+++ Day-13/Q70th.java
@@ -0,0 +1,23 @@
+// Partitioning Into Minimum Number Of Deci-Binary Numbers.
+// Answer will be largest number in String.
+class Q70th {
+    public int minPartitions(String n) {
+        for (char ch = '9'; ch >= '0'; ch--) {
+            if (n.indexOf(ch) != -1) {
+                return ch - '0';
+            }
+        }
+
+        return -1;
+    }
+}
+
+// Input: n = "32"
+// Output: 3
+// Explanation: 10 + 11 + 11 = 32
+
+// Input: n = "82734"
+// Output: 8
+
+// Input: n = "27346209830709182346"
+// Output: 9
