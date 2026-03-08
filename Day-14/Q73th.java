2d76aabc6852409045efa5b324f367ed933cb09a
Khushi Jain <125190832+Khushi-J15@users.noreply.github.com>
2026-03-08 22:39:21 +0530

    method to find different binary string.

--- Day-14/Q73th.java
+++ Day-14/Q73th.java
@@ -1,3 +1,5 @@
+// Given an array of strings nums containing n unique binary strings each of length n, return a binary string of length n that does not appear in nums. If there are multiple answers, you may return any of them.
+
 class Q73th {
     public String findDifferentBinaryString(String[] nums) {
         char[] res = new char[nums.length];
@@ -10,3 +12,7 @@ class Q73th {
         return new String(res);
     }
 }
+
+// Input: nums = ["01","10"]
+// Output: "11"
+// Explanation: "11" does not appear in nums. "00" would also be correct.
